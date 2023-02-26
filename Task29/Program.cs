90ecc063fafc9806be51e997a340a0fe4b365f52
Nadezhda Bereza <a-river@bk.ru>
2023-02-26 10:34:42 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+Console.Clear();
+Console.WriteLine("Задача 29");
+int[] a = new int[8];
+System.Random random = new System.Random();
+bool isFirst = true;
+for (int i=0; i<8; i++) {
+    a[i] = random.Next(1, 100);
+    string prefix = isFirst ? "" : ", ";
+    if (isFirst) { isFirst = false; }
+    Console.Write($"{prefix}{a[i]}");
+}
+Console.WriteLine(" -> [{0}]", string.Join(", ", a));
